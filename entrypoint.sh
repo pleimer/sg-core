7f664e4455702b37336645a7f80081f7c56acbf3
Chris Sibbitt <csibbitt@redhat.com>
2020-04-30 13:52:14 -0400

    Fixing stat_period arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/bridge --stat-period 1 --verbose --gw_unix /smartgateway --amqp_url amqp://stf-default-interconnect.service-telemetry.svc.cluster.local:5672/collectd/telemetry &
+/bridge --stat_period 1 --verbose --gw_unix /smartgateway --amqp_url amqp://stf-default-interconnect.service-telemetry.svc.cluster.local:5672/collectd/telemetry &
 /smart_gateway -promhost 0.0.0.0 unix -path /smartgateway
